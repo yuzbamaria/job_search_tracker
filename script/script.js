665bca57f446afa0bcd1ac6a225614bbd0bed9f3
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2023-12-28 21:40:11 +0000

    added script to script.js for linking the start button in index.html to homepage.html

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    let startBtn = $('#btn-start');
+    // Add event listener to the button Start Journey to link index file to home page
+    startBtn.on('click', function (e) {
+        e.preventDefault();
+        // Redirect the user to the 'homepage.html' page
+        window.location.pathname = '../homepage.html' 
+    });
+});
+
