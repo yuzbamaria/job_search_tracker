8869e1160d513cee18c3e48b2b9f09f747b8717c
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-01-02 20:05:15 +0000

    updated dashboard.js

--- script/dashboard.js
+++ script/dashboard.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     let markers = []; // Array to store markers
     let citiesArray = [];
 
+    let newFile;
+
 
     // Function to store the city list in local storage
     function storeCityList() {
@@ -14,6 +16,8 @@ $(document).ready(function() {
         // If not, you can omit this function
         localStorage.setItem('city-names', JSON.stringify(citiesArray));
     }
+
+
 
     function addMarkerForCity(city) {
         let geocoder = new google.maps.Geocoder();
