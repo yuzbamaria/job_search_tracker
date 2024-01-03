81b1f294196058bcce71f0d8a19be26cc3bf0899
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-01-03 10:53:52 +0000

    fixed the issue with the redirect link on starter page, index.html file

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
+// Adds event listener to homepage Button to redirect to homepage
+let homePageButton = $("#btn-start");
 
-$(document).ready(function () {
-    let startBtn = $('#btn-start');
-console.log(startBtn);
-    // Add event listener to the button Start Journey to link index file to home page
-    startBtn.on('click', function (e) {
-        e.preventDefault();
-        // Redirect the user to the 'homepage.html' page
-        window.location.pathname = '/homepage.html' 
-    });
+// console.log(homePageButton);
+// console.log('click to go to homepage');
+
+homePageButton.on("click", function (e) {
+  e.preventDefault();
+  // Redirects the user to the 'homepage.html' page
+  location.href = "./homepage.html"
 });
